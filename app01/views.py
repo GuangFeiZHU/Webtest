676f91752b573b5795854405579ba90977f6846d
朱广飞 <1464287479@qq.com>
2018-05-18 23:13:41 +0800

    前端初步设计

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
 
 # Create your views here.
+
+
+def index(request):
+    """
+    首页展示
+    :param request:
+    :return:
+    """
+    if request.method == "GET":
+        return render(request,'index.html')
+    else:
+        return Http404()
